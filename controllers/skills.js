e58cf65f970a80dea9b1c248b879744383519f49
mdfasano <mdfasano16@gmail.com>
2023-09-12 16:54:26 -0400

    add a navbar

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,9 @@
-const skillModel = require('../models/skill');
+const SkillModel = require('../models/skill');
 
 function index(req, res, next) {
     res.render('skills/index', {
         title: 'Skills',
-        skillsArray: skillModel.listSkills() 
+        skillsArray: SkillModel.listSkills() 
     })
 }
 
